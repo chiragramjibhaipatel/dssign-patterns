b3595e18988e3c6f268eb3cbdcb1e60556b54509
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2022-11-22 16:04:32 +0530

    construction pattern

---
+++ construction.js
@@ -0,0 +1,24 @@
+let Task = function (name) {
+  this.name = name;
+  this.completed = false;
+
+  this.complete = function () {
+    console.log("completing task: " + this.name);
+    this.completed = true;
+  };
+
+  this.save = function () {
+    console.log("saving task: " + this.name);
+
+  };
+};
+
+let t1 = new Task("demo for constructors");
+let t2 = new Task("demo for modules");
+let t3 = new Task("demo for singletons");
+let t4 = new Task("demo for prototypes");
+
+t1.complete();
+t2.save();
+t3.save();
+t4.save();
